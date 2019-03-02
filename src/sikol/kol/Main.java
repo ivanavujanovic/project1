770564e5a1f0f731b64724f75496ebbad0184024
Ivana Vujanovic <vujanovic.d.ivana@gmail.com>
2019-03-02 12:56:13 +0100

    changed constructors

--- src/sikol/kol/Main.java
+++ src/sikol/kol/Main.java
@@ -9,7 +9,7 @@ public class Main {
 			int brIgraca = 5;
 			Igrac [] igraci = new Igrac [brIgraca];
 			for(int i=0; i< igraci.length;i++){
-				igraci[i] = new Ucenik("ucenik_"+i, igra,100*i, 200+100*i);}
+				igraci[i] = new Ucenik("ucenik12_"+i, igra,100*i, 200+100*i);}
 			Thread.sleep(1000);
 			System.out.println(igra);
 			for(int i=0;i<brIgraca;i++){
